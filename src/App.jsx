d13a0686588738f16df260c32b1e2816658226c7
ian-michael254 <ian.michael.codes@gmail.com>
2025-01-11 15:18:09 +0300

    boiler plate react template

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+const App=()=> {
+  
+
+  return (
+    <>
+      <div className="w-full h-full p-4 bg-red-100">
+        <h1>main application container</h1>
+
+      </div>
+    </>
+  )
+}
+
+export default App
